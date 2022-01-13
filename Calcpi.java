ef84a39140ed590218350d0c1c5c5d0c186e33ee
Sebastian Stenbom <ryezze@gmail.com>
2022-01-13 12:10:30 +0100

    fix notation

--- Calcpi.java
+++ Calcpi.java
@@ -1,26 +1,22 @@
 public class Calcpi{
     public static void main(String[] args){
-        int i;
-        int nThrows = 0;
-        int nSuccess = 0;
+        int points_out = 0;
+        int points_inside = 0;
 
         double x, y; 
 
-        for (i = 0; i < 1000000 ; i++)
+        for (int i = 0; i < 1000000 ; i++)
         {
-           x = Math.random();      // Simulate random Darts
+           x = Math.random();      // Simulate random points
            y = Math.random();      // random between 1 and 0 (exclusive)
-           nThrows++;
+           points_out++;
 
            if ( x*x + y*y <= 1 )
            {
-                nSuccess++;
+                points_inside++;
            }
         
-           System.out.println("Pi = " + 4*(double)nSuccess/(double)nThrows );
+           System.out.println("Pi = " + 4*(double)points_inside/(double)points_out);
         }
-
-        System.out.println("nThrow = " + nThrows );
-        System.out.println("nSuccess = " + nThrows );
     }
 }
